b1d7f319da6d79acba6a691e300c29ba20eb5118
Chris <lcl.chris378@gmail.com>
2022-09-25 16:35:31 +0700

    remove logs

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function changeColors() {
     var colors = ["is-primary", "is-warning", "is-info", "is-danger"];
     var randColor = colors[Math.floor(Math.random() * colors.length)];
 
-    console.log(randColor);
-
     const elem = document.getElementById("chris");
     if (elem.className == "hero is-danger is-fullheight-with-navbar"){
         elem.className = `hero ${randColor} is-fullheight-with-navbar`;
