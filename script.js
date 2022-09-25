61d0cad17bf889b3e25b56d0d979ee812cd75a41
Chris <lcl.chris378@gmail.com>
2022-09-25 16:32:47 +0700

    add favicon

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function changeColors() {
-    var colors = ["is-primary", "is-link", "is-info", "is-danger"];
+    var colors = ["is-primary", "is-warning", "is-info", "is-danger"];
     var randColor = colors[Math.floor(Math.random() * colors.length)];
 
     console.log(randColor);
 
     const elem = document.getElementById("chris");
     if (elem.className == "hero is-danger is-fullheight-with-navbar"){
-        elem.className = "hero "+ randColor +" is-fullheight-with-navbar";
+        elem.className = `hero ${randColor} is-fullheight-with-navbar`;
     } else {
-        elem.className = "hero "+ randColor +" is-fullheight-with-navbar";
+        elem.className = `hero ${randColor} is-fullheight-with-navbar`;
     }
 }
 
